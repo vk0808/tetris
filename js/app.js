f2824fcb70ade7de69bf0077ca905e6d5ccc2abf
VINOD KUMAR R <ravivinod21@gmail.com>
2021-04-05 00:31:11 +0530

    up-next tetromino

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const ScoreDisplay = document.querySelector("#score");
   const StartBtn = document.querySelector("#start-button");
   const width = 10;
+  let nextRandom = 0;
 
   //   the tetrominoes
   const lTetromino = [
@@ -103,10 +104,12 @@ document.addEventListener("DOMContentLoaded", () => {
       );
 
       // start new tetromino
-      random = Math.floor(Math.random() * theTetrominoes.length);
+      random = nextRandom;
+      nextRandom = Math.floor(Math.random() * theTetrominoes.length);
       current = theTetrominoes[random][currentRotation];
       currentPosition = 4;
       draw();
+      displayShape();
     }
   }
 
@@ -152,5 +155,31 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     current = theTetrominoes[random][currentRotation];
     draw();
+  }
+
+  //   show up-next tetromino in mini-grid
+  const displaySquares = document.querySelectorAll(".mini-grid div");
+  const displayWidth = 4;
+  let displayIndex = 0;
+
+  // the Tetromino without rotation
+  const upNextTetrominoes = [
+    [1, displayWidth + 1, displayWidth * 2 + 1, 2], // lTetromino
+    [0, displayWidth, displayWidth * 2, displayWidth * 2 + 1], // zTetromino
+    [1, displayWidth, displayWidth + 1, displayWidth + 2], // tTetromino
+    [0, 1, displayWidth, displayWidth + 1], // oTetromino
+    [1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1], // iTetromino
+  ];
+
+  // display the shape in mini-grid
+  function displayShape() {
+    //   remove any trace of a tetromino form the entire grid
+    displaySquares.forEach((square) => {
+      square.classList.remove("tetromino");
+    });
+
+    upNextTetrominoes[nextRandom].forEach((index) => {
+      displaySquares[displayIndex + index].classList.add("tetromino");
+    });
   }
 });
