02974e6d976a0dd772f3049615ecd292f5aaf215
VINOD KUMAR R <ravivinod21@gmail.com>
2021-04-04 23:40:07 +0530

    rotate tetromino

--- js/app.js
+++ js/app.js
@@ -74,7 +74,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (e.keyCode === 37) {
       moveLeft();
     } else if (e.keyCode === 38) {
-      //rotate
+      rotate();
     } else if (e.keyCode === 39) {
       moveRight();
     } else if (e.keyCode === 40) {
@@ -140,6 +140,17 @@ document.addEventListener("DOMContentLoaded", () => {
     ) {
       currentPosition -= 1;
     }
+    draw();
+  }
+
+  //   rotate the tetromino
+  function rotate() {
+    undraw();
+    currentRotation++;
+    if (currentRotation === current.length) {
+      currentRotation = 0;
+    }
+    current = theTetrominoes[random][currentRotation];
     draw();
   }
 });
