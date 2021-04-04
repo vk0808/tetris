6b9aade29c79ca0111c4932d4daaa2db3319a1f3
VINOD KUMAR R <ravivinod21@gmail.com>
2021-04-04 23:08:42 +0530

    moveLeft function

--- js/app.js
+++ js/app.js
@@ -95,4 +95,21 @@ document.addEventListener("DOMContentLoaded", () => {
       draw();
     }
   }
+
+  //   move the tetromino left, unless is at edge or there is a blockage
+  function moveLeft() {
+    undraw();
+    const isAtLeftEdge = current.some(
+      (index) => (currentPosition + index) % width === 0
+    );
+    if (!isAtLeftEdge) currentPosition -= 1;
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("taken")
+      )
+    ) {
+      currentPosition += 1;
+    }
+    draw();
+  }
 });
