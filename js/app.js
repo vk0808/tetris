64d6aed7d541141bbf25dd6c3e95fac07dfd6824
VINOD KUMAR R <ravivinod21@gmail.com>
2021-04-04 23:18:15 +0530

    other keyCodes

--- js/app.js
+++ js/app.js
@@ -73,6 +73,12 @@ document.addEventListener("DOMContentLoaded", () => {
   function control(e) {
     if (e.keyCode === 37) {
       moveLeft();
+    } else if (e.keyCode === 38) {
+      //rotate
+    } else if (e.keyCode === 39) {
+      //   moveRight();
+    } else if (e.keyCode === 40) {
+      moveDown();
     }
   }
   document.addEventListener("keyup", control);
